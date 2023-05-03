e78adb4b23bf482508f034a41f54b6e430c85bef
Diurno <Diurno@pci208>
2023-05-03 08:57:23 +0200

    Segundo commit

--- Proyecto1/src/prueba1/Principal.java
+++ Proyecto1/src/prueba1/Principal.java
@@ -3,7 +3,8 @@ package prueba1;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("control de versiones con git");
+		System.out.println("control de versiones con git"); 
+		System.out.println("primer cambio");
 	}
 
 }
