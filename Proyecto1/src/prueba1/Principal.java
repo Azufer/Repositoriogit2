d4dbb97e472f9ad6c87f9a91e10daba2f5d48764
Diurno <Diurno@pci208>
2023-05-03 09:43:00 +0200

    Tercer commit

--- Proyecto1/src/prueba1/Principal.java
+++ Proyecto1/src/prueba1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("control de versiones con git"); 
 		System.out.println("primer cambio");
+		System.out.println("segundo cambio");
 	}
 
 }
