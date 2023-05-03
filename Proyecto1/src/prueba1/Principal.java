b77f7ed89f4f7a37e8639fbab0ab8bb6d4b2ccfb
Diurno <Diurno@pci208>
2023-05-03 10:02:07 +0200

    Etiqueta see

--- Proyecto1/src/prueba1/Principal.java
+++ Proyecto1/src/prueba1/Principal.java
@@ -1,7 +1,15 @@
 package prueba1;
-
+/**
+ * 
+ * @author Diurno
+ * @see <a href="https://github.com/Azufer/Repositoriogit2">Repositorio</a>
+ */
 public class Principal {
-
+	
+/**
+ * 
+ * @param args
+ */
 	public static void main(String[] args) {
 		System.out.println("control de versiones con git"); 
 		System.out.println("primer cambio");
